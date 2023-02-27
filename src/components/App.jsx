02bf543523a891033ef00fe86cffe7a2541ac129
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-02-27 15:03:29 +0200

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import Header from './Header/Header';
+import Main from './Main/Main';
+import Footer from './Footer/Footer';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        React homework template
+      </div>
+      <Header>
+        <div></div>
+      </Header>
+      <Main>
+        <div></div>
+      </Main>
+      <Footer>
+        <div></div>
+      </Footer>
+    </>
   );
 };
---
+++ src/components/Header/Headers.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ReviewsBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ReviewsTitle = styled.h2`
+  color: green;
+  margin: auto;
+`;
